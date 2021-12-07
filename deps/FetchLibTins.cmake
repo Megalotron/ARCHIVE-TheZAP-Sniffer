a8d69ff5c13c92d3e505d7b63f136d0e57ec45be
Martin Olivier <martin.olivier@live.fr>
2021-12-07 21:56:58 +0100

    fix: CI

--- deps/FetchLibTins.cmake
+++ deps/FetchLibTins.cmake
@@ -6,8 +6,8 @@ ExternalProject_Add(
         libtins
         GIT_REPOSITORY  https://github.com/mfontanini/libtins.git
         GIT_TAG         origin/master
-        BUILD_COMMAND   ${CMAKE_COMMAND} . -B build/ -DLIBTINS_ENABLE_CXX11=1
-        COMMAND         ${CMAKE_COMMAND} --build build/
+        BUILD_COMMAND   ${CMAKE_COMMAND} -DLIBTINS_ENABLE_CXX11=1 .
+        COMMAND         ${CMAKE_COMMAND} --build .
         INSTALL_COMMAND ""
         TEST_COMMAND    ""
 )
